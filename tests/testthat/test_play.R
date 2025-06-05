82e69f11723906bcb9b2a4cd48f8c7c19447d680
Dhanshree-dhrafani <dhanshree.dhrafani@gmail.com>
2025-06-06 04:51:31 +1000

    Added Rcpp in description, created src folder and added the card_utils.cpp file in the folder

--- tests/testthat/test_play.R
+++ tests/testthat/test_play.R
@@ -5,7 +5,7 @@ test_that("play_game() returns correct structure", {
   expect_named(result, c("winner", "hands", "discard"))
   expect_true(is.character(result$winner) || is.null(result$winner))
   expect_true(is.list(result$hands))
-  expect_s3_class(result$discard, "tbl_df")
+  expect_s3_class(result$discard, "card_vctr")
 })
 
 test_that("play_game() assigns correct number of hands", {
@@ -14,13 +14,15 @@ test_that("play_game() assigns correct number of hands", {
   expect_named(result$hands, paste0("Player_", 1:4))
 })
 
-test_that("play_game() discard pile is valid tibble", {
+test_that("play_game() discard pile is valid card_vctr", {
   result <- play_game(4)
   discard <- result$discard
 
-  expect_s3_class(discard, "tbl_df")
-  expect_true(all(c("color", "value", "type") %in% names(discard)))
-  expect_gt(nrow(discard), 1)
+  expect_s3_class(discard, "card_vctr")
+  expect_gt(length(discard), 1)
+
+  # Optional formatting check
+  expect_true(all(grepl("^[a-z]+_.+$", vctrs::vec_data(discard))))
 })
 
 test_that("play_game() works for different player counts", {
